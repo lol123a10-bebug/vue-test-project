dc7a7fa3c7b9fbe549a9a94ddf871ec7ad8bc74b
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2022-10-26 18:10:48 +0500

    init;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="card-wrapper">
+    <v-card class="card">
+      <v-form ref="form" lazy-validation>
+        <v-text-field
+          label="Email"
+          name="email"
+          type="email"
+          :rules="$v.email.email"
+          v-model.trim.lazy="$v.email.$model"
+        />
+        <v-text-field label="Password" name="password" type="password" />
+        <v-btn>Login</v-btn>
+      </v-form>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import { required, email, minLength } from "vuelidate/lib/validators";
 
 export default Vue.extend({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  name: "Home",
+
+  validations: {
+    email: {
+      required,
+      email: {
+        email,
+      },
+    },
+
+    password: {
+      required,
+      minLength: minLength(6),
+    },
   },
 });
 </script>
+
+<style lang="scss">
+.card {
+  width: 75%;
+  padding: 2rem 0.5rem;
+
+  &-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+}
+</style>
