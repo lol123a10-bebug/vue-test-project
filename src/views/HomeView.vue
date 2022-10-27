e462de5fde100f8aa5bec47cd31e8eb6b9280059
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2022-10-27 05:37:10 +0500

    done;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,50 @@
 <template>
-  <div class="card-wrapper">
-    <v-card class="card">
-      <v-form ref="form" lazy-validation>
+  <v-container fluid class="d-flex align-center justify-center home__container">
+    <v-card class="home__card">
+      <v-form ref="form" lazy-validation @submit.prevent="handleSubmit">
         <v-text-field
+          :error-messages="emailErrors"
           label="Email"
           name="email"
           type="email"
-          :rules="$v.email.email"
-          v-model.trim.lazy="$v.email.$model"
+          v-model.trim="email"
+          required
+          @input="$v.email.$reset()"
+          @blur="$v.email.$reset()"
         />
-        <v-text-field label="Password" name="password" type="password" />
-        <v-btn>Login</v-btn>
+        <v-text-field
+          :error-messages="passwordErrors"
+          label="Password"
+          name="password"
+          type="password"
+          v-model.trim="password"
+          required
+          @input="$v.password.$reset()"
+          @blur="$v.password.$reset()"
+        />
+        <v-btn type="submit">Login</v-btn>
       </v-form>
     </v-card>
-  </div>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { validationMixin } from "vuelidate";
 import { required, email, minLength } from "vuelidate/lib/validators";
 
 export default Vue.extend({
-  name: "Home",
+  mixins: [validationMixin],
+
+  data: () => ({
+    email: "",
+    password: "",
+  }),
 
   validations: {
     email: {
       required,
-      email: {
-        email,
-      },
+      email,
     },
 
     password: {
@@ -36,19 +52,51 @@ export default Vue.extend({
       minLength: minLength(6),
     },
   },
+
+  methods: {
+    handleSubmit(event: SubmitEvent) {
+      this.$v.$touch();
+
+      if (this.$v.$invalid) {
+        return;
+      }
+
+      this.$router.push("/products");
+    },
+  },
+
+  computed: {
+    emailErrors() {
+      const errors: string[] = [];
+      if (!this.$v.email.$dirty) return errors;
+      if (!this.$v.email.email) errors.push("Email should be valid");
+      if (!this.$v.email.required) errors.push("Email is required");
+
+      return errors;
+    },
+
+    passwordErrors() {
+      const errors: string[] = [];
+
+      if (!this.$v.password.$dirty) return errors;
+      if (!this.$v.password.minLength) errors.push("Password should have at least 6 characters");
+      if (!this.$v.password.required) errors.push("Password is required");
+
+      return errors;
+    },
+  },
 });
 </script>
 
-<style lang="scss">
-.card {
-  width: 75%;
-  padding: 2rem 0.5rem;
-
-  &-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+<style lang="scss" scoped>
+.home {
+  &__container {
     height: 100%;
+  }
+
+  &__card {
+    width: 100%;
+    padding: 1rem 0.5rem;
   }
 }
 </style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="product in products" :key="product.id" cols="12" md="4">
+        <v-card height="100%">
+          <v-img height="20rem" :src="product.images[0]" />
+          <v-card-title>{{ product.title }}</v-card-title>
+          <v-card-subtitle>${{ product.price }}</v-card-subtitle>
+          <v-card-text>{{ product.description }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { TProduct } from "@/utils/models/product";
+
+export default Vue.extend({
+  data: () => ({
+    products: [] as TProduct[],
+  }),
+
+  created() {
+    const fetchProducts = async () => {
+      const products = await fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then<TProduct[]>((res) => res.products);
+
+      this.products = products;
+    };
+
+    try {
+      fetchProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
+</script>
